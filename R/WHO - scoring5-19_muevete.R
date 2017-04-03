22c40d0c77d7b571ad116b8ea3414b2c3cb64df6
Manuel A. Bolívar <ma.bolivar643@gmail.com>
2017-04-02 19:44:46 -0500

    Add scoring for T1

--- R/WHO - scoring5-19_muevete.R
+++ R/WHO - scoring5-19_muevete.R
@@ -1,27 +1,27 @@
-#====================================================================
-# WHO - Scoring
-#====================================================================
+#'====================================================================
+#' WHO - Scoring
+#'====================================================================
 
-#====================================================================
-# Set working directory
-#====================================================================
+#'====================================================================
+# Set working directory ----------------------------------------------
+#'====================================================================
 
 setwd("C:/Users/sa.gonzalez68/Dropbox/Ciclovia_Ni?os/Analisis/Bases de datos")
 setwd("E:/Epiandes") # Manuel
 setwd("C:/Users/Manuel/Dropbox/1. Mis documentos/1. Medicina/Epiandes - Scoring")
 
-#====================================================================
-# Load libraries
-#====================================================================
+#'====================================================================
+# Load libraries -----------------------------------------------------
+#'====================================================================
 
 library(haven)
 library(dplyr)
 library(lubridate)
 library(readr)
 
-#====================================================================
-# WHo
-#====================================================================
+#'====================================================================
+# WHO ----------------------------------------------------------------
+#'====================================================================
 
 wfawho2007<-read.table("WHO_igrowup5-19\\wfawho2007.txt",header=T,sep="",skip=0)
 hfawho2007<-read.table("WHO_igrowup5-19\\hfawho2007.txt",header=T,sep="",skip=0)
@@ -29,9 +29,9 @@ bfawho2007<-read.table("WHO_igrowup5-19\\bfawho2007.txt",header=T,sep="",skip=0)
 
 source("WHO_igrowup5-19\\who2007.r")
 
-#====================================================================
-# Scoring for baseline - muevete
-#====================================================================
+#'===================================================================
+# Scoring for baseline - muevete ------------------------------------
+#'===================================================================
 
 df <- read_csv("./data/muevete0/muevete0.csv",
                col_types = cols(
@@ -70,6 +70,46 @@ who2007(FilePath=getwd(),
 
 read_csv("./output/Z-scores_muevete0_2017-01-22_z.csv")
 
-#====================================================================
-# Scoring for .... - muevete
-#====================================================================
+#'===================================================================
+# Scoring for T1 - muevete -------------------------------------------
+#'===================================================================
+
+df_t1 <- read_csv("./data/muevete1/muevete1.csv",
+               col_types = cols(
+                 Id_3 = col_character(),
+                 Sexo = col_integer(),
+                 Dob = col_character(), # Day of birth?
+                 Dom = col_character(), # Day of measure?
+                 Weight_3 = col_double(),
+                 Height_3 = col_double(),
+                 Oedema = col_character(),
+                 Sw = col_integer()
+                 #Measure = col_character()
+               )) %>% 
+  mutate(AGE_MONTHS = time_length(mdy(Dom)-mdy(Dob),unit = "month"),
+         MEASURE = "H") %>%
+  select(1,Id_3,Sexo,AGE_MONTHS,Weight_3,Height_3,Oedema,Sw) %>%
+  rename(ID_3 = Id_3,
+         SEX = Sexo,
+         WEIGHT = Weight_3,
+         HEIGHT = Height_3,
+         OEDEMA = Oedema,
+         SW = Sw) %>%
+  as.data.frame()
+
+names(df_t1)[1] <- "FECHA_3"
+
+who2007(FilePath=getwd(), 
+        FileLab= paste0("./output/Z-scores_muevete1_",Sys.Date()),
+        mydf=df_t1,
+        sex=SEX,
+        age=AGE_MONTHS,
+        weight=WEIGHT, 
+        height=HEIGHT,
+        #measure=MEASURE,
+        oedema=OEDEMA,
+        sw = SW)
+
+
+read_csv("./output/Z-scores_muevete1_2017-02-18_z.csv") %>%
+  write_csv("./output/Z-scores_muevete1_2017-02-18_z.csv",na = ".")
